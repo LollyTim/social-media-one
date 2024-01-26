0c3dc125ed057f90d3027a7dec833c7aea4b53a0
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-26 16:23:18 +0100

    soind some setups

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Login = (props) => {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
+
+export default Login
